875d2ba7fb3435b09afcf072bfa964229976a338
Moritz311 <20172030@atn.ac.at>
2025-11-28 16:05:32 +0100

    scheiß swift

--- Uno/Uno/ImmersiveView.swift
+++ Uno/Uno/ImmersiveView.swift
@@ -33,39 +33,66 @@ struct ImmersiveView: View {
         Card(color: .red, number: "six"),
         Card(color: .red, number: "seven"),
         Card(color: .red, number: "eight"),
-        Card(color: .red, number: "nine"),
+        Card(color: .blue, number: "nine"),
+        Card(color: .red, number: "out"),
+        Card(color: .red, number: "reverse"),
+        Card(color: .green, number: "plus2"),
     ]
 
     var body: some View {
         RealityView { content in
-            // Add the initial RealityKit content
-            if let immersiveContentEntity = try? await Entity(named: "UnoCard", in: realityKitContentBundle) {
-                var manipulator = ManipulationComponent()
-                manipulator.releaseBehavior = .stay
-                manipulator.dynamics.scalingBehavior = .none
-                
-                var cardEntity = immersiveContentEntity.findEntity(named: "UnoCard")
-                var test = cardEntity?.findEntity(named: "Cube_1")
-                test?.components[ModelComponent.self]?.materials = [SimpleMaterial(color: .green, isMetallic: false)]
-                
-                var xOffset: Float = 0.01
-                cardEntity?.components.set(manipulator)
-                
-                var cloned = cardEntity?.clone(recursive: true)
-                cloned?.position.x = xOffset
-                test = cloned?.findEntity(named: "Cube_1")
-                test?.components[ModelComponent.self]?.materials = [SimpleMaterial(color: .green, isMetallic: false)]
-                cloned?.components.set(manipulator)
-                
-                xOffset += 0.01
-                
-                
-                for card in cards {
-                    
-                }
-                
-                content.add(immersiveContentEntity)
-            }
+            if let rootEntity = try? await Entity(named: "UnoCard",
+                                                             in: realityKitContentBundle),
+                           let originalCard = rootEntity.findEntity(named: "UnoCard") {
+
+                            var manipulator = ManipulationComponent()
+                            manipulator.releaseBehavior = .stay
+                            manipulator.dynamics.scalingBehavior = .none
+
+                            for card in cards {
+                                    
+                                let cardClone = originalCard.clone(recursive: true)
+
+                                var xOffset: Float = 0.1
+                                cardClone.position = [xOffset, 0, -1]
+
+                                if let face = cardClone.findEntity(named: "Cube_1"),
+                                   var model = face.components[ModelComponent.self] {
+                                    model.materials = [
+                                        SimpleMaterial(
+                                            color: card.color,
+                                            isMetallic: false
+                                        )
+                                    ]
+                                   face.components.set(model)
+                                }
+                                
+                                if let cylinder = cardClone.findEntity(named: "Cylinder"),
+                                   var model = cylinder.findEntity(named: "usdPrimitiveAxis")?.components[ModelComponent.self]
+                                {
+
+                                    do {
+                                            let newMaterial = try await MaterialResource.load(
+                                                named: card.number,              // "one", "two", ...
+                                                in: realityKitContentBundle
+                                            )
+
+                                            model.materials = [newMaterial]
+                                            cylinder.components.set(model)
+
+                                        } catch {
+                                            print("Material \(card.number) konnte nicht geladen werden:", error)
+                                        }
+                                }
+                                
+
+
+                                cardClone.components.set(manipulator)
+
+                                content.add(cardClone)
+                                xOffset += 0.1
+                            }
+                        }
         }
     }
 }
--- Uno/Uno/ToggleImmersiveSpaceButton.swift
+++ Uno/Uno/ToggleImmersiveSpaceButton.swift
@@ -49,7 +49,7 @@ struct ToggleImmersiveSpaceButton: View {
                 }
             }
         } label: {
-            Text(appModel.immersiveSpaceState == .open ? "Hide Immersive Space" : "Show Immersive Space")
+            Text(appModel.immersiveSpaceState == .open ? "Quit" : "Play")
         }
         .disabled(appModel.immersiveSpaceState == .inTransition)
         .animation(.none, value: 0)
